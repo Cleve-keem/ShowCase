a24fd6a6b27850e87ecec022a7b7df312a56f81a
hakeem <belloharyourmidey@gmail.com>
2024-10-30 00:57:02 -0700

    committing today

--- src/Components/Showcase.jsx
+++ src/Components/Showcase.jsx
@@ -1,40 +1,80 @@
 import styles from "./Showcase.module.css";
-import image1 from "white_jordan (1).png";
-import image2 from "white_jordan (2).png";
-import image3 from "white_jordan (3).png";
+import image1 from "../assets/shoes/white_jordan1.png";
+import image2 from "../assets/shoes/white_jordan2.png";
+import image3 from "../assets/shoes/white_jordan3.png";
+import image4 from "../assets/shoes/white_jordan4.png";
+import image5 from "../assets/shoes/black_jordan1.png";
+import image6 from "../assets/shoes/black_jordan2.png";
+import image7 from "../assets/shoes/black_jordan3.png";
+import { useState } from "react";
 
 const images = [
     image1,
     image2,
-    image3
+    image3,
+    image4,
+    image5,
+    image6,
+    image7
 ]
 
-import { useState } from "react";
+const item_images = [
+    image1,
+    image2,
+    image3,
+    image4
+]
 
-function Showcase({ item_images, shoe_image}) {
+const themes = {
+    purple: '#ba0ad1',
+    light: '#eee'
+}
+
+function Showcase() {
 
     const [ currentIndex, setCurrentIndex ] = useState(0);
+    const [ bgColor, setBgColor ] = useState('#fff');
+    const [activeTheme, setActiveTheme] = useState("light");
 
     const handleNextButton = () => {
-        setCurrentIndex((prevIndex) => prevIndex + 1);
+        setCurrentIndex((prevIndex) => prevIndex === images.length - 1 ? 0 :  prevIndex + 1);
     }
+
+    const handlePrevButton = () => {
+        setCurrentIndex((prevIndex) => prevIndex === 0 ? images.length - 1 : prevIndex - 1);
+    }
+
+    const changeTheme = (theme) => {
+        setBgColor(themes[theme]);
+        setActiveTheme(theme);
+    };
 
     return (
         <div className="container">
             <div className={styles.showcase_wrapper}>
-                <div className={styles.showcase}>
+                <div className={styles.showcase} style={{backgroundColor: bgColor}}>
                     {/* slide image */}
                     <div className={styles.slide_show}>
                         <div className={styles.slide_item}>
-                            <img className={styles.slide_img} src={images[image1]} alt="slide Image" />
-                            <div className={styles.arrow_left}><i className="ri-arrow-left-s-line"></i></div>
-                            <div className={styles.arrow_right}><i className="ri-arrow-right-s-line"></i></div>
+                            <img className={styles.slide_img} src={images[currentIndex]} alt="slide Image" />
+                            <button 
+                                className={styles.arrow_left}
+                                onClick={handlePrevButton}><i className="ri-arrow-left-s-line"></i></button>
+                            <button 
+                                className={styles.arrow_right}
+                                onClick={handleNextButton}><i className="ri-arrow-right-s-line"></i></button>
                         </div>
                     </div>
                     {/* themes */}
                     <div className={styles.slide_themes}>
-                        <span className={`${styles.dark_themes} active`}></span>
-                        <span className={styles.light_themes}></span>
+                        <span
+                        className={`${styles.purple_theme} ${activeTheme === "purple" ? styles.active : ""}`}
+                        onClick={() => changeTheme("purple")}
+                        ></span>
+                        <span
+                        className={`${styles.light_theme} ${activeTheme === "light" ? styles.active : ""}`}
+                        onClick={() => changeTheme("light")}
+                        ></span>
                     </div>
 
                     {/* Buttons and Price */}
@@ -53,11 +93,11 @@ function Showcase({ item_images, shoe_image}) {
                 {/* Preview section  */}
                 <div className={styles.preview}>
                     <ul className={styles.items}>
-                        {item_images && (
-                            <li className={`${styles.preview_item} flex`}>
-                                <img src="shoes\white_jordan_3.png" alt="previewed image" />
+                        {item_images.map((item, index) => (
+                            <li key={index} className={`${styles.preview_item} flex`}>
+                                <img src={item} alt="previewed image" />
                             </li>
-                        )}
+                        ))}
                     </ul>
                 </div>
             </div>
